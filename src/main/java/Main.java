1c77b2b5ce2aff7b53bfa0d72485e5dcd58e0fcb
BEER <beermumu@hotmail.com>
2018-12-24 00:10:58 +0700

    login and register finish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Slazapee");
-        primaryStage.setScene(new Scene(root, 960, 720));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
