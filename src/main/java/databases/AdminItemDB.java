02a24bbfe4ae33cf89a437ef8d99b51e09a95995
Yackz <40462476+Yackz@users.noreply.github.com>
2018-12-23 19:13:04 +0700

    AdminItem

--- src/main/java/databases/AdminItemDB.java
+++ src/main/java/databases/AdminItemDB.java
@@ -2,6 +2,7 @@ package databases;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import models.Item;
 
 import java.sql.*;
 
@@ -28,19 +29,22 @@ public class AdminItemDB {
         }
     }
     public static ObservableList loadItem() {
-        ObservableList<RegisterSubject> accounts = FXCollections.observableArrayList();
+        ObservableList<Item> items = FXCollections.observableArrayList();
         try {
             Class.forName(dbName);
             Connection connection = DriverManager.getConnection(dbURL);
             if (connection != null) {
-                String query = "select * from Register";
+                String query = "select * from Item";
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
                 while (resultSet.next()) {
                     String id = resultSet.getString("ID");
+                    String type = resultSet.getString("Type");
                     String name = resultSet.getString("Name");
-                    int credit = resultSet.getInt("Credit");
-                    accounts.add(new RegisterSubject(id,name,credit));
+                    int quantity = resultSet.getInt("Quantity");
+                    int cost = resultSet.getInt("Cost");
+                    String description = resultSet.getString("Description");
+                    items.add(new Item(id,type,name,quantity,cost,description));
                 }
                 statement.close();
                 resultSet.close();
@@ -51,7 +55,43 @@ public class AdminItemDB {
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        return accounts;
+        return items;
+    }
+
+    public static void editItem(String id, String type , String name, int quantity , int cost , String description) {
+        try {
+            Class.forName(dbName);
+            Connection connection = DriverManager.getConnection(dbURL);
+            if (connection != null) {
+                String query = "update Item set ID = '" + id + "' , " +
+                        "Type = '" + type + "' , Name = '" + name + "' , Quantity = '" + quantity + "' , Cost = '" + cost +"' " +
+                        ", Description = '" + description + "' where Subject.ID =='" + id + "'";
+                PreparedStatement p = connection.prepareStatement(query);
+                p.executeUpdate();
+                connection.close();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteItem(String id) {
+        try {
+            Class.forName(dbName);
+            Connection connection = DriverManager.getConnection(dbURL);
+            if (connection != null) {
+                String query = "Delete from Item where ID == '" + id + "'";
+                PreparedStatement p = connection.prepareStatement(query);
+                p.executeUpdate();
+                connection.close();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
