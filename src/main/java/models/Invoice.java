6f48a8723aeada69f8d1e30a9faea5fc25bb3c49
Yackz <40462476+Yackz@users.noreply.github.com>
2018-12-23 18:07:33 +0700

    change models

---
+++ src/main/java/models/Invoice.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Invoice {
+    private String id;
+    private String date;
+    private int status;
+    private String accountID;
+
+    public Invoice(String id, String date, int status, String accountID) {
+        this.id = id;
+        this.date = date;
+        this.status = status;
+        this.accountID = accountID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+}
